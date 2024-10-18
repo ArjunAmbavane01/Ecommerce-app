ed0c42e49490c7ffea5f0506471f4d94bd369ee1
Arjun <arjun.ambavane@gmail.com>
2024-10-18 11:13:59 +0530

    Signup Frontend Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Welcome from './pages/Welcome';
 import LaptopWelcome from './pages/laptop/LaptopWelcome';
 import LaptopStore from './pages/laptop/LaptopStore';
 import CustomizePC from './pages/pc/CustomizePC';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/laptops" element={<LaptopWelcome />} />
         <Route path="/laptops/store" element={<LaptopStore />} />
         <Route path="/customize-pc" element={<CustomizePC />} />
+        <Route path="/cart" element={<ShoppingCart />} />
       </Routes>
     </Router>
   );
